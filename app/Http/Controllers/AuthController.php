9b8d40e53f3114d3657fb9ab6cf8c0a0931a249e
derTurke <gurhan061@gmail.com>
2022-03-27 19:04:17 +0300

    Forgot Password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -97,6 +97,53 @@ class AuthController extends Controller
         ],Response::HTTP_OK);
     }
 
+    public function forgotPassword(Request $request){
+        $email = $request->input('email');
+        if (empty($email)){
+            return response()->json('E-posta Adresi boş olamaz!');
+        }
+        $users = DB::table('users')->where('email',$email)->first();
+        if (!empty($users->email)){
+            return response()->json('success');
+        } else {
+            return response()->json('Girilmiş olan e-posta adresine kayıtlı üyelik bulunmamaktadır. Lütfen üye olunuz!');
+        }
+    }
+    public function forgotPassword2(Request $request){
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $password_again = $request->input('password_again');
+        if (empty($password)){
+            return response()->json([
+                'message' => 'Şifre boş olamaz!'
+            ]);
+        }
+        if ($password != $password_again){
+            return response()->json([
+                'message' => 'Şifreler birbirleriyle eşleşmemektedir. Lütfen şifrenizi kontrol ediniz!'
+            ]);
+        }
+        $update = DB::table('users')->where('email',$email)->update([
+           'password' => Hash::make($password)
+        ]);
+        if (!empty($update)){
+            if(!Auth::attempt($request->only('email','password'))){
+                return response()->json([
+                    'message' => 'Bilgiler yanlış lütfen kontrol ediniz!'
+                ],Response::HTTP_UNAUTHORIZED);
+            }
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'message' => 'Giriş işlemi başarıyla gerçekleşti.'
+            ],Response::HTTP_OK);
+        } else {
+            return response()->json([
+                'message' => 'Şifreniz güncellenirken bir sorunla karşılaşıldı. Lütfen tekrar deneyiniz!'
+            ]);
+        }
+    }
     public function logout(){
         auth()->user()->tokens()->delete();
         return response(['message' => 'Başarıyla çıkış yaptınız!']);
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
+Route::post('forgotPassword',[AuthController::class,'forgotPassword']);
+Route::post('forgotPassword2',[AuthController::class,'forgotPassword2']);
 
 Route::middleware('auth:sanctum')->group(function(){
 
